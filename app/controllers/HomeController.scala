3e6e19927ccc13cac4eb25999aa1d7d48fff46ee
Matheus Bastos <matheusbrc@hotmail.com>
2018-11-26 20:14:13 +0000

    36. criação de arquivo e outras alterações

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -9,24 +9,9 @@ import models.tbCliente
 import models.tbAutor
 import models.tbLivro
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(db: Database, cc: ControllerComponents)
 extends AbstractController(cc) with play.api.i18n.I18nSupport {
-
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
   
   def sC() = Action {
     val list_sC = MutableList[tbCliente]()

e80efa979986d98b564c42b48921c2245c13f2d1
Matheus Bastos <matheusbrc@hotmail.com>
2018-11-26 20:57:32 +0000

    40. preenchimento de arquivo e outras alterações

--- app/models/ClienteDAO.scala
+++ app/models/ClienteDAO.scala
@@ -4,15 +4,31 @@ import play.api.db.Database;
 
 object ClienteDAO{
     
-    def getCliente(db: Database, cdCliente: Int): Cliente = {
+    def getCliente(db: Database, cdCliente: Int): tbCliente = {
         db.withConnection{conn =>
             val ps = conn.prepareStatement("select * from tb_cliente where cd_cliente=?")
             ps.setInt(1,cdCliente)
             val res = ps.executeQuery()
             if(res.next())
-                Cliente(res.getString(2),res.getString(3))
+                tbCliente(res.getInt(1),res.getString(2),res.getString(3))
             else
-                Cliente("","")
+                tbCliente(0,"","")
         }
+    }
+    
+    def listCliente(db: Database): MutableList[tbCliente] = {
+    
+        val list_sC = MutableList[tbCliente]()
+    
+        db.withConnection { conn =>
+          val stm = conn.createStatement()
+          val res = stm.executeQuery("""select * from cliente""")
+          while (res.next()) {
+            list.+=(tbCliente(res.getInt(1)
+                   ,res.getString(2)
+                   ,res.getString(3)))
+          }
+        }
+        list
     }
 }

2f511fad4dc6e525a2217dc7f1b03747e45d8bc9
Matheus Bastos <matheusbrc@hotmail.com>
2018-11-26 18:03:38 +0000

    15. importação

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,6 +4,7 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.db.Database
+import scala.collection.mutable.MutableList
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
